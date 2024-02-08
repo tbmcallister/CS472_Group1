a7867f0621ac342d54e57a26bfb89d78c528aa8e
tbmcallister <tbmcallister2002@gmail.com>
2024-02-07 18:33:13 -0800

    tdd commit #2

---
+++ tdd/src/counter.py
@@ -0,0 +1,62 @@
+from flask import Flask
+
+# we need to import the file that contains the status codes
+from src import status
+
+app = Flask(__name__)
+
+COUNTERS = {}
+
+
+# We will use the app decorator and create a route called slash counters.
+# specify the variable in route <name>
+# let Flask know that the only methods that is allowed to be called
+# on this function is "POST".
+@app.route('/counters/<name>', methods=['POST'])
+def create_counter(name):
+    """Create a counter"""
+    app.logger.info(f"Request to create counter: {name}")
+    global COUNTERS
+
+    if name in COUNTERS:
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+
+    COUNTERS[name] = 0
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
+
+# create a route for method PUT on endpoint /counters/<name>
+# create a function to implement that route
+@app.route('/counters/<name>', methods=['PUT'])
+def update_counter(name):
+    """ Update the counter by incrementing it by 1"""
+    # increment the counter by 1
+    global COUNTERS
+
+    if name in COUNTERS:
+        # update counter and return the counter
+        COUNTERS[name] += 1
+        app.logger.info(f"Updating counter {name} by 1 to a value of {COUNTERS[name]}")
+
+        app.logger.info(f"Returning counter {name} with a value of {COUNTERS[name]}")
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+    # if no counter found
+    return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+
+
+# create a route for method GET on endpoint /counters/<name>
+# create a function to implement that route
+@app.route('/counters/<name>', methods=['GET'])
+def read_counter(name):
+    """ Read and return the value of the counter"""
+    global COUNTERS
+
+    if name in COUNTERS:
+        # return the value of the counter
+        app.logger.info(f"Returning counter {name} with a value of {COUNTERS[name]}")
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+    # if no counter found
+    return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+
---
+++ tdd/tests/test_counter.py
@@ -0,0 +1,92 @@
+"""
+Test Cases for Counter Web Service
+
+Create a service that can keep a track of multiple counters
+- API must be RESTful - see the status.py file. Following these guidelines, you can make assumptions about
+how to call the web service and assert what it should return.
+- The endpoint should be called /counters
+- When creating a counter, you must specify the name in the path.
+- Duplicate names must return a conflict error code.
+- The service must be able to update a counter by name.
+- The service must be able to read the counter
+"""
+from unittest import TestCase
+# we need to import the unit under test - counter
+from src.counter import app
+# we need to import the file that contains the status codes
+from src import status
+
+
+class CounterTest(TestCase):
+    """Counter tests"""
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_create_a_counter(self):
+        """It should create a counter"""
+        result = self.client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def test_update_a_counter(self):
+        counterName = "foo1"
+        # make a call to create a counter
+        result = self.client.post(f'/counters/{counterName}')
+
+        # ensure that it returned a successful return code
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+        # check the counter value as a baseline
+        baselineResponse = self.client.get(f'/counters/{counterName}')
+        baselineVal = baselineResponse.json[counterName]
+        app.logger.info(f"Counter {counterName} has a value of {baselineVal} before updating")
+
+        # make a call to update the counter that you just created
+        result = self.client.put(f'/counters/{counterName}')
+
+        # ensure that it returned a successful return code
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+        # check that the counter value is one more than the baseline
+        updatedResponse = self.client.get(f'/counters/{counterName}')
+        updatedVal = updatedResponse.json[counterName]
+        app.logger.info(f"Counter {counterName} has a value of {updatedVal} after updating")
+
+        self.assertEqual(updatedVal, baselineVal+1)
+
+    def test_read_a_counter(self):
+        counterName = "foo2"
+        # make a call to create a counter
+        result = self.client.post(f'/counters/{counterName}')
+
+        # make sure the counter was correctly created
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+        # read the value of the counter, which should be 0
+        result = self.client.get(f'/counters/{counterName}')
+
+        # ensure that it returned a successful return code
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+        # check to see if the value of the counter is 0
+        app.logger.info(f"Counter {counterName} has a value of {result}")
+
+    def test_update_a_bad_counter(self):
+        # this will test a counter that doesn't exist
+        result = self.client.put('/counters/badcounter')
+
+        # check that it is correctly failing
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_read_a_bad_counter(self):
+        # this will test a counter that doesn't exist
+        result = self.client.get('/counters/badcounter')
+
+        # check that it is correctly failing
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
